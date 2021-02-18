01dbb187baf423e04017246df12c616f469dc4c5
Enrique Prieto <enriqueneto65@gmail.com>
2021-02-18 19:29:22 -0300

    Inserido chave da api e credenciais do firebase no projeto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  HGKey: "d98f4063",
+  firebase: {
+    apiKey: "AIzaSyBI6sn8POQqm9AZpXWNaHDskauGmZpfCGU",
+    authDomain: "franq-28d43.firebaseapp.com",
+    projectId: "franq-28d43",
+    storageBucket: "franq-28d43.appspot.com",
+    messagingSenderId: "596058391798",
+    appId: "1:596058391798:web:00873e46785a6de67218d7"
+  }
 };
 
 /*
